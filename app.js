382326e8f0a555d1c925182974ca371d5fc2244c
piyush <piyush@gmail.com>
2023-09-05 13:51:09 +0530

    bugs fixed

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get('/*', function (req, res) {
   path.join(__dirname, './dist/index.html'),
   function (err) {
     if (err) {
-     res.status(500).send(err);
+     res.status(500).send('eeee');
     }
   }
 )})
