f9962e30f47d8454deb1af204080f8ad7035d76a
piyush <piyush@gmail.com>
2023-09-20 15:51:18 +0530

    new Build

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ app.use("/api/v1", require("./app/api/v1/routes")(express));
 
 // Starting Server
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 
 app.listen(port,()=>{
